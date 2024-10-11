2aa7d947f6bbe01b84627d9ef7b4d66a106e69e0
Hongfei Yan <yanhf@pku.edu.cn>
2024-10-11 18:29:44 +0800

    initial

---
+++ code/testing_code.py
@@ -0,0 +1,47 @@
+import subprocess
+import difflib
+import os
+import sys
+
+def test_code(script_path, infile, outfile):
+    command = ["python", script_path]  # 使用Python解释器运行脚本
+    with open(infile, 'r') as fin, open(outfile, 'r') as fout:
+        expected_output = fout.read().strip()
+        # 启动一个新的子进程来运行指定的命令
+        process = subprocess.Popen(command, stdin=fin, stdout=subprocess.PIPE)
+        actual_output, _ = process.communicate()
+        if actual_output.decode().strip() == expected_output:
+            return True
+        else:
+            print(f"Output differs for {infile}:")
+            diff = difflib.unified_diff(
+                expected_output.splitlines(),
+                actual_output.decode().splitlines(),
+                fromfile='Expected', tofile='Actual', lineterm=''
+            )
+            print('\n'.join(diff))
+            return False
+
+
+if __name__ == "__main__":
+    # 检查命令行参数的数量
+    if len(sys.argv) != 2:
+        print("Usage: python testing_code.py <filename>")
+        sys.exit(1)
+
+    # 获取文件名
+    script_path = sys.argv[1]
+
+    #script_path = "class.py"  # 你的Python脚本路径
+    #test_cases = ["d.in"]  # 输入文件列表
+    #expected_outputs = ["d.out"]  # 预期输出文件列表
+    # 获取当前目录下的所有文件
+    files = os.listdir('.')
+
+    # 筛选出 .in 和 .out 文件
+    test_cases = [f for f in files if f.endswith('.in')]
+    expected_outputs = [f for f in files if f.endswith('.out')]
+
+    for infile, outfile in zip(test_cases, expected_outputs):
+        if not test_code(script_path, infile, outfile):
+            break
